011aa70a077ec1926cbd55cda30ec4324781f351
Yaroslav Pratsovity <ypratsovit@e1r6p3.unit.ua>
2020-06-15 12:41:00 +0300

    simple_send_resv_json

--- client/src/main.c
+++ client/src/main.c
@@ -4,12 +4,10 @@ int main(int argc, char *argv[]) {
    int sockfd, portno, n;
    struct sockaddr_in serv_addr;
    struct hostent *server;
-   char buffer[256];
+   char buffer[2048];
    int rc = 0;
+   char *send_data = NULL;
 
-   /////////
-   //mx_interface(argc, argv);
-   ///////
    if (argc < 3) {
       fprintf(stderr,"usage %s hostname port\n", argv[0]);
       exit(0);
@@ -17,7 +15,6 @@ int main(int argc, char *argv[]) {
 
    portno = atoi(argv[2]);
 
-   /* Create a socket point */
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&rc, sizeof(int));
 
@@ -38,45 +35,39 @@ int main(int argc, char *argv[]) {
    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
    serv_addr.sin_port = htons(portno);
 
-   /* Now connect to the server */
    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
       perror("ERROR connecting");
       exit(1);
    } else
       printf("%s", "Connected!\n");
 
-   /* Now ask for a message from the user, this message
-      * will be read by server
-   */
-
 while(true) {
    printf("%s", ": ");
-   bzero(buffer,256);
-   fgets(buffer,255,stdin);
+   bzero(buffer,2048);
+   fgets(buffer,2048,stdin);
    json_object *jobj = json_object_new_object();
-   json_object *jstring = json_object_new_string("Joys of Programming");
-   json_object *jint = json_object_new_int(10);
-   json_object *jboolean = json_object_new_boolean(1);
-   json_object *jdouble = json_object_new_double(2.14);
-   json_object_object_add(jobj,"Site Name", jstring);
-   json_object_object_add(jobj,"Technical blog", jboolean);
-   json_object_object_add(jobj,"Average posts per day", jdouble);
-   json_object_object_add(jobj,"Number of posts", jint);
+   json_object *j_type = json_object_new_string("Logging");
+   json_object *j_login = json_object_new_string("Vasya");
+   json_object *j_passwd = json_object_new_string("123qwert");
+   json_object_object_add(jobj,"Type", j_type);
+   json_object_object_add(jobj,"Login", j_login);
+   json_object_object_add(jobj,"Passwd", j_passwd);
+
    // const char *test;
    // struct json_object *parsed;
    // struct json_object *name;
    /* Send message to the server */
-   printf("%s", json_object_to_json_string(jobj));
-   n = send(sockfd, jobj, sizeof(jobj), false);
+   send_data = (char *)json_object_to_json_string(jobj);
+
+   n = send(sockfd, send_data, strlen(send_data), 0);
 
    if (n < 0) {
       perror("ERROR writing to socket");
       exit(1);
    }
-
    /* Now read server response */
-   bzero(buffer,256);
-   n = recv(sockfd, buffer, 255, false);
+   bzero(buffer, 2048);
+   n = recv(sockfd, buffer, 2048, 0);
 
    if (n <= 0) {
       printf("%s","Lost connection with the server\n");
@@ -87,7 +78,6 @@ while(true) {
          sleep(3);
       }
    }
-
    printf("%s", buffer);
 }
    return 0;
