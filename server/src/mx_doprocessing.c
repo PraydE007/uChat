b1491b60861fb1d90ce4b6de31ab47426fcff45d
Yaroslav Pratsovity <ypratsovit@e1r6p3.unit.ua>
2020-06-25 08:39:34 +0300

    add_socket_to_json

--- server/src/mx_doprocessing.c
+++ server/src/mx_doprocessing.c
@@ -12,7 +12,7 @@ void *mx_doprocessing (void *data) {
     char buffer[2048];
     json_object *jobj = json_object_new_object();
     json_object *j_result = json_object_new_object();
-
+    json_object *j_socket = json_object_new_int(sockbd.sockfd);
 
     while (true) {
         bzero(buffer,2048);
@@ -21,12 +21,13 @@ void *mx_doprocessing (void *data) {
             if_disconnect(sockbd);
             break;
         }
+        jobj = json_tokener_parse(buffer);
+        json_object_object_add(jobj,"Socket", j_socket);
         write (sockbd.log_sescr, "Taked ", 6);
-        write (sockbd.log_sescr, buffer, strlen(buffer));
+        write (sockbd.log_sescr, json_object_to_json_string(jobj), strlen(json_object_to_json_string(jobj)));
         write (sockbd.log_sescr, " from socket: ", 14);
         write (sockbd.log_sescr, mx_itoa(sockbd.sockfd), 1);
         write (sockbd.log_sescr, "\n", 1);
-        jobj = json_tokener_parse(buffer);
         j_result = mx_dbase_handler(jobj, sockbd.bd); //
         //json_object_put(jobj);
         //printf("json_object_to_json_string(j_result): %s\n", json_object_to_json_string(j_result)); //
