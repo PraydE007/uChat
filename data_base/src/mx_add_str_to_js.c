1454151fbf94dd7331bcefd0f4e5fc578e322c20
Anatolii Kostanda <akostanda@e1r7p3.unit.ua>
2020-07-07 16:19:48 +0300

    1st push of if_send_message

--- data_base/src/mx_add_str_to_js.c
+++ data_base/src/mx_add_str_to_js.c
@@ -1,6 +1,6 @@
 #include "dbase.h"
 
-void mx_js_add_str(struct json_object *j_result, char *key, char *value) {
+void mx_add_str_to_js(struct json_object *j_result, char *key, char *value) {
     json_object *j_answer = json_object_new_string(value);
     json_object_object_add(j_result, key, j_answer);
 }
--- data_base/src/mx_js_arr_add.c
+++
@@ -1,5 +0,0 @@
-#include "dbase.h"
-
-void mx_js_arr_add(struct json_object *j_result, struct json_object *j_answer) {
-    json_object_array_add(j_result, j_answer);
-}
