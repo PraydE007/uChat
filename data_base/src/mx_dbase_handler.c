f8b6e1d68d232d97d5287cc27448836efa32015c
Anatolii Kostanda <akostanda@e1r7p3.unit.ua>
2020-07-07 12:14:02 +0300

    for pull client change to add contact to profile

--- data_base/src/mx_dbase_handler.c
+++ data_base/src/mx_dbase_handler.c
@@ -3,7 +3,7 @@
 json_object *mx_dbase_handler(json_object *jobj, sqlite3 *db) {
     t_datab *datab = mx_create_datab_node();
     json_object *j_result = json_object_new_object();
-    json_object *j_result2 = json_object_new_object();
+    // json_object *j_result2 = json_object_new_object();
 
     if (!mx_strcmp("Logging", mx_json_to_str(jobj, "Type"))) {
         j_result = mx_if_logging(jobj, db, datab);
@@ -15,15 +15,15 @@ json_object *mx_dbase_handler(json_object *jobj, sqlite3 *db) {
         // json_object_object_add(jobj,"Security_key", j_test_Security_key);
         // j_result2 = mx_if_chat(jobj, db, datab);
         // printf("BUFFER: %s\n", json_object_get_string(j_result2));
-        json_object *j_test_Login = json_object_new_string("AAA");
-        json_object *j_test_Chat_name = json_object_new_string("Chisto potrindetb");
-        // json_object *j_test_Security_key = json_object_new_string("cdef9e974ae775ea9b1472cedb108607127607e538b5c1ff8f9e461234");
-        json_object_object_add(jobj,"Login", j_test_Login);
-        json_object_object_add(jobj,"Message", j_test_Chat_name);
-        // json_object_object_add(jobj,"Security_key", j_test_Security_key);
-        printf("!!!(jobj): %s\n", json_object_to_json_string(jobj));
-        j_result2 = mx_if_send_message(jobj, db, datab);
-        printf("BUFFER: %s\n", json_object_get_string(j_result2));
+        // json_object *j_test_Login = json_object_new_string("AAA");
+        // json_object *j_test_Chat_name = json_object_new_string("Chisto potrindetb");
+        // // json_object *j_test_Security_key = json_object_new_string("cdef9e974ae775ea9b1472cedb108607127607e538b5c1ff8f9e461234");
+        // json_object_object_add(jobj,"Login", j_test_Login);
+        // json_object_object_add(jobj,"Message", j_test_Chat_name);
+        // // json_object_object_add(jobj,"Security_key", j_test_Security_key);
+        // printf("!!!(jobj): %s\n", json_object_to_json_string(jobj));
+        // j_result2 = mx_if_send_message(jobj, db, datab);
+        // printf("BUFFER: %s\n", json_object_get_string(j_result2));
     }
     else if (!mx_strcmp("Registr", mx_json_to_str(jobj, "Type")))
         j_result = mx_if_registr(jobj, db, datab);
