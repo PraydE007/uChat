083b2d565d5b9db2eb3aaf683e5925ea4b5b0a18
Anatolii Kostanda <akostanda@e1r7p3.unit.ua>
2020-07-07 16:59:15 +0300

    1st push of if_send_message

--- data_base/src/mx_dbase_handler.c
+++ data_base/src/mx_dbase_handler.c
@@ -3,7 +3,7 @@
 json_object *mx_dbase_handler(json_object *jobj, sqlite3 *db) {
     t_datab *datab = mx_create_datab_node();
     json_object *j_result = json_object_new_object();
-    // json_object *j_result2 = json_object_new_object();
+    json_object *j_result2 = json_object_new_object();
 
     if (!mx_strcmp("Logging", mx_js_to_str(jobj, "Type"))) {
         j_result = mx_if_logging(jobj, db, datab);
