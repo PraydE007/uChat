a3ad234077078a1e11dcf3031fbfd1d2e1ad1d70
Yaroslav Pratsovity <ypratsovit@e1r6p1.unit.ua>
2020-07-11 17:32:09 +0300

    fixing_chats

--- client/src/mx_open_chat.c
+++ client/src/mx_open_chat.c
@@ -1,17 +1,5 @@
 #include "client.h"
 
-static bool what_chat(t_s_glade *gui) {
-    int count = 0;
-
-    if (gui->chats)
-        while (gui->chats[count]) {
-            if(!mx_strcmp(gui->chats[count], gui->send_to))
-                return true;
-            count++;
-        }
-        return false;
-}
-
 void mx_open_chat(GtkListBox *box, GtkListBoxRow *row, gpointer data) {
     const char *send_data = NULL;
     t_s_glade *gui = (t_s_glade *)data;
@@ -24,10 +12,7 @@ void mx_open_chat(GtkListBox *box, GtkListBoxRow *row, gpointer data) {
     gui->send_to = strdup(gtk_label_get_text(GTK_LABEL(chat_name)));
 
     json_object *jobj = json_object_new_object();
-    if (what_chat(gui))
-        j_type = json_object_new_string("Public_chat");
-    else
-        j_type = json_object_new_string("Private_chat");
+    j_type = json_object_new_string("Chat");
     json_object *j_login = json_object_new_string(login);
     json_object *j_chat_name = json_object_new_string(gui->send_to);
     json_object *j_key = json_object_new_string(gui->key);
--- client/src/mx_open_group.c
+++ client/src/mx_open_group.c
@@ -6,34 +6,21 @@ void mx_open_group(GtkButton *btn, gpointer data) {
     const gchar *group_name = gtk_entry_get_text(GTK_ENTRY(gui->e_find));
     const char *send_data = NULL;
     char *login = (char *)gtk_entry_get_text(GTK_ENTRY(gui->e_l_login));
-    bool success_create = true;
-    int count = 0;
 
     (void)btn;
         if (mx_strcmp(group_name, "")) {
-            if (gui->contacts) {
-            while (gui->contacts[count]) {
-                if (!mx_strcmp(gui->contacts[count], (char *)group_name)) {
-                    success_create = false;
-                    break;
-                }
-                count++;
-            }
-            }
-            if (success_create) {
-                json_object *jobj = json_object_new_object();
-                json_object *j_type = json_object_new_string("Create_chat");
-                json_object *j_login = json_object_new_string(login);
-                json_object *j_chat_name = json_object_new_string(group_name);
-                json_object *j_key = json_object_new_string(gui->key);
-                json_object_object_add(jobj,"Type", j_type);
-                json_object_object_add(jobj,"Login", j_login);
-                json_object_object_add(jobj,"Chat_name", j_chat_name);
-                json_object_object_add(jobj,"Security_key", j_key);
-                send_data = (char *)json_object_to_json_string(jobj);
-                printf("SEND_DATA :%s\n", send_data);
-                send(gui->sockfd, send_data, strlen(send_data), 0);
-            }
+            json_object *jobj = json_object_new_object();
+            json_object *j_type = json_object_new_string("Create_chat");
+            json_object *j_login = json_object_new_string(login);
+            json_object *j_chat_name = json_object_new_string(group_name);
+            json_object *j_key = json_object_new_string(gui->key);
+            json_object_object_add(jobj,"Type", j_type);
+            json_object_object_add(jobj,"Login", j_login);
+            json_object_object_add(jobj,"Chat_name", j_chat_name);
+            json_object_object_add(jobj,"Security_key", j_key);
+            send_data = (char *)json_object_to_json_string(jobj);
+            printf("SEND_DATA :%s\n", send_data);
+            send(gui->sockfd, send_data, strlen(send_data), 0);
         }
         // else {
         // OPEN GROUP CONTROL FOR SELECTED GROUP
