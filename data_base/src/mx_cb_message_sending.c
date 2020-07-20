66ef6c017b298d83cd0ee628469f1465f9ecaa16
Anatolii Kostanda <akostanda@e1r7p3.unit.ua>
2020-07-20 14:57:47 +0300

    for pull file send

--- data_base/src/mx_cb_message_sending.c
+++ data_base/src/mx_cb_message_sending.c
@@ -10,9 +10,11 @@ int mx_cb_message_sending(void *datab, int argc, char **argv,
 
         n = send(mx_atoi(argv[0]), new_datab->message_db,
                                         mx_strlen(new_datab->message_db), 0);
-        if (new_datab->fpath_db)
+        if (new_datab->fpath_db) {
+            printf("fpath_db = %s\n", new_datab->fpath_db);
+            printf("argv[0]!!! = %s\n", argv[0]);
             mx_send_image_from_server(new_datab->fpath_db, mx_atoi(argv[0]), mx_atoi(new_datab->fsize_db));
-        printf("fpath_db = %s\n", new_datab->fpath_db);
+        }
     }
     return 0;
 }
