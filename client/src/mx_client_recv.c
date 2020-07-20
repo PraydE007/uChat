3ca9cad7495830c0df0eed486e5741e2345da9af
Yaroslav Pratsovity <ypratsovit@e1r6p1.unit.ua>
2020-07-20 18:29:55 +0300

    fixing_auditor

--- client/src/mx_client_recv.c
+++ client/src/mx_client_recv.c
@@ -1,10 +1,31 @@
 #include "client.h"
 
+static void first_handle(char *answer, t_s_glade *gui, json_object *jobj) {
+    if (!mx_strcmp(answer, "Private message history!")) {
+        gui->if_contact = 1;
+        gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_history, gui, 0);
+    }
+    if (!mx_strcmp(answer, "Public message history!")) {
+        gui->if_contact = 0;
+        gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_history, gui, 0);
+    }
+    if (!mx_strcmp(answer, "You have logged in!")) {
+        gui->if_contact = 2;
+        gui->key = (char *)json_object_get_string(json_object_object_get(jobj, "Security_key"));
+        printf("%s\n", gui->recv_data);
+        gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_logging, gui, 0);
+    }
+    if (!mx_strcmp(answer, "You have registered successfully!"))
+        gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_registr, gui, 0);
+    if (!mx_strcmp(answer, "Profile info!"))
+        gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_profile, gui, 0);
+}
+
 void *mx_client_recv(void *data) {
     int n = 0;
     t_s_glade *gui = *(t_s_glade **)data;
     char *answer = NULL;
-    json_object *jobj;
+    json_object *jobj = NULL;
 
     while (true) {
         bzero(gui->buffer, MX_MAX_BYTES);
@@ -18,24 +39,7 @@ void *mx_client_recv(void *data) {
         gui->recv_data = strdup(gui->buffer);
         printf("RECV DATA!!!: %s\n", gui->recv_data);
         answer = (char *)json_object_get_string(json_object_object_get(jobj, "Answer"));
-        if (!mx_strcmp(answer, "Private message history!")) {
-            gui->if_contact = 1;
-            gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_history, gui, 0);
-        }
-        if (!mx_strcmp(answer, "Public message history!")) {
-            gui->if_contact = 0;
-            gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_history, gui, 0);
-        }
-        if (!mx_strcmp(answer, "You have logged in!")) {
-            gui->if_contact = 2;
-            gui->key = (char *)json_object_get_string(json_object_object_get(jobj, "Security_key"));
-            printf("%s\n", gui->recv_data);
-            gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_logging, gui, 0);
-        }
-        if (!mx_strcmp(answer, "You have registered successfully!"))
-            gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_registr, gui, 0);
-        if (!mx_strcmp(answer, "Profile info!"))
-            gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_profile, gui, 0);
+        first_handle(answer, gui, jobj);
         if (!mx_strcmp(answer, "Profile data are changed!"))
             gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_change_profile, gui, 0);
         if (!mx_strcmp(answer, "The password were changed!"))
@@ -51,9 +55,6 @@ void *mx_client_recv(void *data) {
             mx_recieve_image(gui);
             gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_render_img, gui, 0);
         }
-        if (!mx_strcmp(answer, "Public_receive_file"))
-            mx_recieve_image(gui);
-            //gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_render_img, gui, 0);
         if (!mx_strcmp(answer, "The chat was created!"))
             gdk_threads_add_idle_full(G_PRIORITY_HIGH_IDLE, mx_success_add_contact, gui, 0);
         if (!mx_strcmp(answer, "The user was added to the chat!"))
--- client/src/mx_success_history.c
+++ client/src/mx_success_history.c
@@ -7,24 +7,35 @@ static const char *json_to_str(json_object *jobj, char *key) {
     return value;
 }
 
+static t_buf_history init_buf_history() {
+    t_buf_history buf_history;
+
+    buf_history.array = NULL;
+    buf_history.size_messages = 0;
+    buf_history.get_message = NULL;
+    buf_history.get_owner = NULL;
+    return buf_history;
+}
+
 gboolean mx_success_history(void *data) {
     t_s_glade *gui = (t_s_glade *)data;
+    t_buf_history buf_history = init_buf_history();
     json_object *jobj = json_tokener_parse(gui->recv_data);
-    const char *array = json_to_str(jobj, "Array");
-    json_object *j_arr = json_tokener_parse(array);
-    int size_messages = json_object_array_length(j_arr);
-    char *get_message = NULL;
-    char *get_owner = NULL;
-    for (int i = 0; i < size_messages; i++) {
+
+    buf_history.array = json_to_str(jobj, "Array");
+    json_object *j_arr = json_tokener_parse(buf_history.array);
+    buf_history.size_messages = json_object_array_length(j_arr);
+    for (int i = 0; i < buf_history.size_messages; i++) {
         json_object *arr_idx = json_object_array_get_idx(j_arr, i);
-            get_message = (char *)json_to_str(arr_idx, "Message");
-        if (!get_message) {
-            get_message = (char *)json_to_str(arr_idx, "Own_message");
-            get_owner = (char *)json_to_str(arr_idx, "Login");
-            mx_p_own(gui->l_messages, get_message, get_owner);
+            buf_history.get_message = (char *)json_to_str(arr_idx, "Message");
+        if (!buf_history.get_message) {
+            buf_history.get_message = (char *)json_to_str(arr_idx, "Own_message");
+            buf_history.get_owner = (char *)json_to_str(arr_idx, "Login");
+            mx_p_own(gui->l_messages,
+            buf_history.get_message, buf_history.get_owner);
         }
         else
-            mx_p_owned(gui->l_messages, get_message, gui->send_to);
+            mx_p_owned(gui->l_messages, buf_history.get_message, gui->send_to);
     }
     return 0;
 }
