751af139f0262bbb30abcdcdd18f36c48bfd289b
Anatolii Kostanda <akostanda@e1r7p3.unit.ua>
2020-07-21 17:31:16 +0300

    modified doprocesing for protect from another client

--- data_base/src/mx_dealloc_datab.c
+++ data_base/src/mx_dealloc_datab.c
@@ -1,6 +1,8 @@
 #include "dbase.h"
 
 void mx_dealloc_datab(t_datab **datab) {
+    (*datab)->logtrigger = (*datab)->passtrigger = 0;
+    (*datab)->socket = (*datab)->lenth = 0;
     mx_strdel(&((*datab)->id));
     mx_strdel(&((*datab)->second_id));
     mx_strdel(&((*datab)->chat_id));
@@ -12,9 +14,8 @@ void mx_dealloc_datab(t_datab **datab) {
     mx_strdel(&((*datab)->type));
     mx_strdel(&((*datab)->security_key));
     (*datab)->chat_name_db = (*datab)->email_db = NULL;
-    (*datab)->mobile_db = NULL;
-    (*datab)->password_db = NULL;
-    (*datab)->security_db = NULL;
+    (*datab)->mobile_db = (*datab)->password_db = NULL;
+    (*datab)->security_db = (*datab)->buffer_db = NULL;
     (*datab)->message_db = NULL;
     (*datab)->fsize_db = (*datab)->fpath_db = NULL;
     (*datab)->db = NULL;
