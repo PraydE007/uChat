f982c3311e288d9aab4550197e0e221f36195f49
Yaroslav Pratsovity <ypratsovit@e1r6p1.unit.ua>
2020-07-21 19:00:12 +0300

    some_fix

--- client/src/mx_success_add_contact.c
+++ client/src/mx_success_add_contact.c
@@ -5,8 +5,10 @@ gboolean mx_success_add_contact(void *data) {
 
     char *contact = (char *)gtk_entry_get_text(GTK_ENTRY(gui->e_find));
     if (gui->sender) {
-        if(mx_strcmp(gui->sender, contact))
+        if(mx_strcmp(gui->sender, contact)) {
             mx_push_chat(gui->l_chats, contact);
+            free(gui->sender);
+        }
         else {
             mx_show_dialog(gui->w_chat,
             "You have already this contact in the list!");
